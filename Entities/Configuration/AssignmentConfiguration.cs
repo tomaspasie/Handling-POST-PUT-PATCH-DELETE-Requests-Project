2b14a534ba85470a4f6587e27c19e5e255ba5b64
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2021-04-13 05:27:32 -0400

    Added Requests

---
+++ Entities/Configuration/AssignmentConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Entities.Configuration
+{
+    public class AssignmentConfiguration : IEntityTypeConfiguration<Assignment>
+    {
+        public void Configure(EntityTypeBuilder<Assignment> builder)
+        {
+            builder.HasData
+            (
+                new Assignment
+                {
+                    Id = new Guid("c9d4c053-49b6-410c-bc78-2d54a9991873"),
+                    AssignmentName = "Assignment 1",
+                },
+                new Assignment
+                {
+                    Id = new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"),
+                    AssignmentName = "Assignment 2",
+                }
+            );
+        }
+    }
+}
---
+++ Entities/DataTransferObjects/AssignmentDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DataTransferObjects
+{
+    public class AssignmentDto
+    {
+        public Guid Id { get; set; }
+        public string AssignmentName { get; set; }
+    }
+}
